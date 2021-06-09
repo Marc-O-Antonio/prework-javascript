2931df07004d21b5bf2c60d4d52c3cf64d11bc02
Antek <marmaros987@gmail.com>
2021-06-09 21:59:56 +0200

    add game result

--- js/script.js
+++ js/script.js
@@ -16,30 +16,12 @@ function getMoveName(argMoveId){
     return 'nieznany ruch';
   }
 
-let randomNumber = Math.floor(Math.random() * 3 + 1);
-
-console.log('Wylosowana liczba to: ' + randomNumber);
-
-let argComputerMove = getMoveName(randomNumber);
-
-printMessage('Mój ruch to: ' + argComputerMove);
-
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
-console.log('Gracz wpisał: ' + playerInput);
-
-let argPlayerMove = getMoveName(playerInput);
-
-printMessage('Twój ruch to: ' + argPlayerMove);
-
-
-/*
-function displayResult(argComputerMove, argPlayerMove){
+  function displayResult(argComputerMove, argPlayerMove){
     printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
-PRZY DODANIU FUNKCJI NIE WYŚWIETLA WYNIKU KILKA H TO ROZKMINIAŁEM */
+
     if(argComputerMove == 'kamień' && argPlayerMove == 'papier'){
-        printMessage('Ty wygrywasz!');
-      } else if (argComputerMove == 'kamień' && argPlayerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+  } else if (argComputerMove == 'kamień' && argPlayerMove == 'nożyce'){
       printMessage('Wygrałem z Tobą, tym razem.');
   } else if (argComputerMove == 'kamień' && argPlayerMove == 'kamień'){
       printMessage('Mamy remis! Zagrajmy jeszcze raz');
@@ -58,6 +40,27 @@ PRZY DODANIU FUNKCJI NIE WYŚWIETLA WYNIKU KILKA H TO ROZKMINIAŁEM */
   } else {
       printMessage('Przykro mi ale nierozpoznałem Twojego ruchu. Spróbuj jeszcze raz wpisać liczbę 1 (kamień) lub 2 (papier) albo 3 (nożyce).');
   } 
-/*
+
 }
-*/
+
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let argComputerMove = getMoveName(randomNumber);
+
+printMessage('Mój ruch to: ' + argComputerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let argPlayerMove = getMoveName(playerInput);
+
+printMessage('Twój ruch to: ' + argPlayerMove);
+
+
+
+
+
+displayResult ();
