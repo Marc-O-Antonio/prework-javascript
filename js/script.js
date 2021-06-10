57bc19f31b1795549dbca78474861ab9b14b2537
Antek <marmaros987@gmail.com>
2021-06-10 15:06:52 +0200

    add game loop(10) & game win rate manipulation

--- js/script.js
+++ js/script.js
@@ -47,23 +47,40 @@ function playGame (playerInput){
 
     }
 
-    let randomNumber = Math.floor(Math.random() * 3+ 1);
 
-    console.log('Wylosowana liczba to: ' + randomNumber);
+
+    /* console.log('Wylosowana liczba to: ' + randomNumber);
 
     let argComputerMove = getMoveName(randomNumber);
 
-    printMessage('Mój ruch to: ' + argComputerMove); 
+    */
 
     /* let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.'); */
 
     console.log('Gracz wpisał: ' + playerInput);
 
     let argPlayerMove = getMoveName(playerInput);
-  
+    let randomNumber = Math.floor(Math.random() * 4 + 1);
 
+    if (randomNumber == 4) {
+        if ( argPlayerMove == 'papier'){
+            argComputerMove = 'nożyce'
+        }
+        else if (argPlayerMove == 'nożyce') {
+            argComputerMove = 'kamień'
+        } else {argComputerMove = 'papier'}
+    } else {
+        if ( argPlayerMove == 'papier') {
+            argComputerMove = 'kamień'
+        } else if (argPlayerMove == 'nożyce') {
+            argComputerMove = 'papier'
+        } else {argComputerMove = 'nożyce'}  
+    }
 
     printMessage('Twój ruch to: ' + argPlayerMove);
+    printMessage('Mój ruch to: ' + argComputerMove);
+
+    console.log ('Ruch komputera to ' + argComputerMove);
 
 
 
@@ -86,3 +103,7 @@ function playGame (playerInput){
   
   testButton.addEventListener('click', function(){playGame(3)});
 
+
+  for (let i=0; i<10; i++){
+      playGame(1)
+  }
